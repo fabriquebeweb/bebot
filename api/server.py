f272c59ba4a1be48ab56f22ae4146ba2e241a45f
atlasrw <atlasrw@icloud.com>
2021-07-15 03:22:04 +0000

    CMD

--- api/server.py
+++ api/server.py
@@ -7,13 +7,13 @@ app = FastAPI()
 
 @app.get('/cc/{id}')
 def getCodecademyProfile(id: str):
-    os.system(f'rm -f scrap/cc.json && scrapy parse https://www.codecademy.com/profiles/{id} --spider=codecademy -o scrap/cc.json:json')
-    return json.loads(open('scrap/cc.json').read())[0]
+    os.system(f'rm -f tmp/cc.json && scrapy parse https://www.codecademy.com/profiles/{id} --spider=codecademy -o tmp/cc.json:json')
+    return json.loads(open('tmp/cc.json').read())[0]
 
 @app.get('/sl/{id}')
 def getSololearnProfile(id: str):
-    os.system(f'rm -f scrap/sl.json && scrapy parse https://www.sololearn.com/profile/{id} --spider=sololearn -o scrap/sl.json:json')
-    return json.loads(open('scrap/sl.json').read())[0]
+    os.system(f'rm -f tmp/sl.json && scrapy parse https://www.sololearn.com/profile/{id} --spider=sololearn -o tmp/sl.json:json')
+    return json.loads(open('tmp/sl.json').read())[0]
 
 def bootstrap():
     uvicorn.run('api.server:app', host='0.0.0.0', port=5000, reload=True)
