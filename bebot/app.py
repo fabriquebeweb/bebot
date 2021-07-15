9ec68f522b4011624f8c99e677b88c08c16fdd0e
atlasrw <atlasrw@icloud.com>
2021-07-15 07:03:10 +0000

    APP

--- bebot/app.py
+++ bebot/app.py
@@ -1,11 +1,26 @@
+import asyncio
 import discord
+import uvicorn
+import requests as req
+from fastapi import FastAPI
 from dotenv import dotenv_values
 
+app = FastAPI()
 ENV = dotenv_values()
 bebot = discord.Client()
+
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(bebot.start(ENV['TOKEN']))
 
 @bebot.event
 async def on_ready():
     print('HELLO WORLD!')
+    print(req.get('https://w3schools.com'))
 
-bebot.run(ENV['TOKEN'])
+@app.get("/")
+async def read_root():
+    return {"Hello": str(bebot.user)}
+
+def bootstrap():
+    uvicorn.run('bebot.app:app', host='0.0.0.0', port=5555, reload=True)
